90121d70d798b9c5ae7f748a76a6125797876d51
Frederick Ankomah <ankomahfrederickyahoo@gmail.com>
2024-10-03 14:29:44 +0000

    Add logic to quiz.js file

---
+++ quiz.js
@@ -0,0 +1,14 @@
+const submit = document.getElementById('submit-answer');
+
+submit.addEventListener("click", checkAnswer);
+
+function checkAnswer(){
+    let feedBack = document.getElementById('feedback');
+    let correctAnswer = "4";
+    let userAnswer = document.querySelector('input[name="quiz"]').value;
+    if (userAnswer === correctAnswer){
+        feedBack.innerText="Correct! Well done.";
+    }else {
+        feedBack.innerText="That's incorrect. Try again!";
+    }
+}
